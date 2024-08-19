8164e28dffec60af20195e0ae45958a71c714f0c
Akira Durham <akiradurham@yahoo.com>
2024-08-19 13:41:35 -0400

    working multithreaded drawing and animation, choppy

--- sort.py
+++ sort.py
@@ -3,12 +3,11 @@ This module creates a visualizer for sorting algorithms
 """
 import matplotlib.pyplot as plt
 import numpy as np
-import threading
 from matplotlib.animation import FuncAnimation
 
 AMOUNT = 30 # how many nums are generated
-PAUSE = 0.0005 # how long matplotlib waits before moving again
-plt.figure(figsize=(12, 10)) # size of window
+PAUSE = 100 # how long matplotlib waits before moving again
+
 
 nums_bubble = np.arange(1, AMOUNT + 1)
 nums_selection = np.arange(1, AMOUNT + 1)
@@ -21,13 +20,6 @@ np.random.shuffle(nums_selection)
 np.random.shuffle(nums_quick)
 np.random.shuffle(nums_insertion)
 np.random.shuffle(nums_merge)
-
-x = np.arange(0, AMOUNT, 1)
-bars_bubble = plt.bar(x, nums_bubble, color='blue', width = 0.6)
-bars_selection = plt.bar(x, nums_selection, color='blue', width = 0.6)
-bars_quick = plt.bar(x, nums_quick, color='blue', width = 0.6)
-bars_insertion = plt.bar(x, nums_insertion, color='blue', width = 0.6)
-bars_merge = plt.bar(x, nums_merge, color='blue', width = 0.6)
 
 # Bubble sort
 # Bubbles items up to where they need to be in list, stable, in-place
@@ -58,11 +50,11 @@ def selection(bars, nums):
         #     bar.set_color('blue')
         # bars[i].set_color('red')
         for j in range(i+1, AMOUNT):
-            bars[j].set_color('black')
+            # bars[j].set_color('black')
             # plt.pause(PAUSE) 
             if nums[j] < nums[min_val]:
                 min_val = j
-            bars[j].set_color('blue')
+            # bars[j].set_color('blue')
         nums[i], nums[min_val] = nums[min_val], nums[i]
         yield nums
     #     plt.pause(PAUSE)
@@ -84,25 +76,29 @@ def count_frames(generator):
         count += 1
     return count
 
-frames_bubble = count_frames(bubble(bars_bubble, nums_bubble))
-frames_selection = count_frames(selection(bars_selection, nums_selection))
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 10))
+# bars_quick = plt.bar(x, nums_quick, color='blue', width = 0.6)
+# bars_insertion = plt.bar(x, nums_insertion, color='blue', width = 0.6)
+# bars_merge = plt.bar(x, nums_merge, color='blue', width = 0.6)
 
 ax1.set_title('Bubble Sort')
 ax1.set_xlabel('Index')
 ax1.set_ylabel('Value')
-bubble_gen = bubble(bars_bubble, nums_bubble)
+bars_bubble = ax1.bar(np.arange(AMOUNT), nums_bubble, color='blue', width = 0.6)
 
 ax2.set_title('Selection Sort')
 ax2.set_xlabel('Index')
 ax2.set_ylabel('Value')
+bars_selection = ax2.bar(np.arange(AMOUNT), nums_selection, color='blue', width = 0.6)
+
+bubble_gen = bubble(bars_bubble, nums_bubble)
+
 selection_gen = selection(bars_selection, nums_selection)
 
-ani_bubble = FuncAnimation(fig, update_bubble, frames=bubble_gen, repeat=False, interval=PAUSE, blit=True, save_count=frames_bubble)
-ani_selection = FuncAnimation(fig, update_selection, frames=selection_gen, repeat=False, interval=PAUSE, blit=True, save_count=frames_selection)
+ani_bubble = FuncAnimation(fig, update_bubble, frames=bubble(bars_bubble, nums_bubble), repeat=False, interval=PAUSE, blit=True)
+ani_selection = FuncAnimation(fig, update_selection, frames=selection(bars_selection, nums_selection), repeat=False, interval=PAUSE, blit=True)
 
-plt.tight_layout()
 plt.show()
 
 # Bubble sort
