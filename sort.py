6f0cca86a4af37fbb154ba8678810ea152407a83
Akira Durham <akiradurham@yahoo.com>
2024-08-19 14:54:14 -0400

    added in insertio sort and made 2x3 grid

--- sort.py
+++ sort.py
@@ -8,7 +8,6 @@ from matplotlib.animation import FuncAnimation
 AMOUNT = 30 # how many nums are generated
 PAUSE = 100 # how long matplotlib waits before moving again
 
-
 nums_bubble = np.arange(1, AMOUNT + 1)
 nums_selection = np.arange(1, AMOUNT + 1)
 nums_quick = np.arange(1, AMOUNT + 1)
@@ -48,10 +47,27 @@ def selection(bars, nums):
             bar.set_color('blue')
         bars[i].set_color('red')
         for j in range(i+1, AMOUNT):
-            bars[j].set_color('gray')
             if nums[j] < nums[min_val]:
                 min_val = j
         nums[i], nums[min_val] = nums[min_val], nums[i]
+        yield nums
+
+# Insertion sort
+# Moves down the list and moves all larger items down one, then puts item in first item mvoed spot, stable, in-place
+# Time complexity : best O(n), avg O(n^2), worst O(n^2)
+# Space complexity : O(1)
+def insertion(bars, nums):
+    for i in range(1, AMOUNT):
+        for bar, height in zip(bars, nums):
+            bar.set_height(height)
+            bar.set_color('blue')
+        bars[i].set_color('red')
+        val = i
+        j = i -1
+        while j >= 0 and val < nums[j]:
+            nums[j + 1] = nums[j]
+            j -= 1
+        nums[j + 1] = val
         yield nums
 
 def update_bubble(frame):
@@ -64,48 +80,56 @@ def update_selection(frame):
         bar.set_height(height)
     return bars_selection
 
-def count_frames(generator):
-    count = 0
-    for _ in generator:
-        count += 1
-    return count
+def update_insertion(frame):
+    for bar, height in zip(bars_insertion, frame):
+        bar.set_height(height)
+    return bars_insertion
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+fig, ax = plt.subplots(2, 3, figsize=(12, 8))
 
-# bars_quick = plt.bar(x, nums_quick, color='blue', width = 0.6)
-# bars_insertion = plt.bar(x, nums_insertion, color='blue', width = 0.6)
-# bars_merge = plt.bar(x, nums_merge, color='blue', width = 0.6)
+# bars_quick = plt.bar(np.arange(AMOUNT), nums_quick, color='blue', width = 0.6)
+# bars_merge = plt.bar(np.arange(AMOUNT), nums_merge, color='blue', width = 0.6)
 
-ax1.set_title('Bubble Sort')
-ax1.set_xlabel('Index')
-ax1.set_ylabel('Value')
-bars_bubble = ax1.bar(np.arange(AMOUNT), nums_bubble, color='blue', width = 0.6)
+ax[0,0].set_title('Bubble Sort')
+ax[0,0].set_xlabel('Index')
+ax[0,0].set_ylabel('Value')
+bars_bubble = ax[0,0].bar(np.arange(AMOUNT), nums_bubble, color='blue', width = 0.6)
 
-ax2.set_title('Selection Sort')
-ax2.set_xlabel('Index')
-ax2.set_ylabel('Value')
-bars_selection = ax2.bar(np.arange(AMOUNT), nums_selection, color='blue', width = 0.6)
+ax[0,1].set_title('Selection Sort')
+ax[0,1].set_xlabel('Index')
+ax[0,1].set_ylabel('Value')
+bars_selection = ax[0,1].bar(np.arange(AMOUNT), nums_selection, color='blue', width = 0.6)
+
+ax[0,2].set_title('Insertion Sort')
+ax[0,2].set_xlabel('Index')
+ax[0,2].set_ylabel('Value')
+bars_insertion = ax[0,2].bar(np.arange(AMOUNT), nums_insertion, color='blue', width = 0.6)
 
 bubble_gen = bubble(bars_bubble, nums_bubble)
-
 selection_gen = selection(bars_selection, nums_selection)
+insertion_gen = insertion(bars_insertion, nums_insertion)
 
 ani_bubble = FuncAnimation(fig, update_bubble, frames=bubble(bars_bubble, nums_bubble), repeat=False, interval=PAUSE, blit=True, cache_frame_data=False)
 ani_selection = FuncAnimation(fig, update_selection, frames=selection(bars_selection, nums_selection), repeat=False, interval=PAUSE, blit=True, cache_frame_data=False)
+ani_insertion = FuncAnimation(fig, update_insertion, frames=insertion(bars_insertion, nums_insertion), repeat=False, interval=PAUSE, blit=True, cache_frame_data=False)
+
+fig.suptitle('Sorting Algorithms Visualizer', fontsize=16)
 
 plt.show()
 
-# Bubble sort
+
+
+# Merge sort
 # Bubbles items up to where they need to be in list, stable, in-place
 # Time complexity : best O(n), avg O(n^2), worst O(n^2)
 # Space complexity : O(1)
 
-# Bubble sort
+# Quick sort
 # Bubbles items up to where they need to be in list, stable, in-place
 # Time complexity : best O(n), avg O(n^2), worst O(n^2)
 # Space complexity : O(1)
 
-# Bubble sort
+# Heap sort
 # Bubbles items up to where they need to be in list, stable, in-place
 # Time complexity : best O(n), avg O(n^2), worst O(n^2)
 # Space complexity : O(1)
